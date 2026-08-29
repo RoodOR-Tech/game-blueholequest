2de6dc8589835f6cde1d06787c345824b699767e
RoodOR-Tech <roodj.or@gmail.com>
2026-08-29 13:54:02 -0700

    chore: add src/app/config.ts

---
+++ src/app/config.ts
@@ -0,0 +1,29 @@
+import Phaser from 'phaser';
+import { BootScene } from '../scenes/BootScene';
+import { TitleScene } from '../scenes/TitleScene';
+
+export const WORLD_WIDTH = 256;
+export const WORLD_HEIGHT = 240;
+
+export const gameConfig: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: 'game',
+  width: WORLD_WIDTH,
+  height: WORLD_HEIGHT,
+  backgroundColor: '#08111d',
+  pixelArt: true,
+  roundPixels: true,
+  scene: [BootScene, TitleScene],
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: WORLD_WIDTH,
+    height: WORLD_HEIGHT,
+  },
+  render: {
+    antialias: false,
+    pixelArt: true,
+    roundPixels: true,
+  },
+};
+
