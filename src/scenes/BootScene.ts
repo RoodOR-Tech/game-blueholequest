a07640b1283d73d4ae3a355c6b4e585b46caf2ba
RoodOR-Tech <roodj.or@gmail.com>
2026-08-29 14:47:43 -0700

    feat: add meaningful calamity choices and animated Dad

--- src/scenes/BootScene.ts
+++ src/scenes/BootScene.ts
@@ -1,11 +1,21 @@
 import Phaser from 'phaser';
+import {
+  DAD_SPRITE_PATH,
+  DAD_TEXTURE_KEY,
+  registerDadAnimations,
+} from '../actors/dadAnimations';
 
 export class BootScene extends Phaser.Scene {
   constructor() {
     super('boot');
   }
 
+  preload(): void {
+    this.load.image(DAD_TEXTURE_KEY, DAD_SPRITE_PATH);
+  }
+
   create(): void {
+    registerDadAnimations(this);
     this.scene.start('title');
   }
 }
