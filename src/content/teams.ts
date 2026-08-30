a076435d7e45926ffd5ef34cba2cad147bd22f1b
RoodOR-Tech <roodj.or@gmail.com>
2026-08-30 16:00:49 -0700

    Align family heroes with corrected reference

--- src/content/teams.ts
+++ src/content/teams.ts
@@ -12,6 +12,7 @@ export interface TeamDefinition {
   readonly displayName: string;
   readonly weaponId: string;
   readonly passiveId: string;
+  readonly passiveDescription: string;
   readonly startingStats: TeamStats;
   readonly productionReady: boolean;
 }
@@ -21,23 +22,26 @@ export const TEAMS: readonly TeamDefinition[] = [
     id: 'dad_paula',
     displayName: 'Dad & Paula',
     weaponId: 'threaded_plumbing_wrench',
-    passiveId: 'master_craftsman',
+    passiveId: 'master_plumber',
+    passiveDescription: 'Smashes standard blocks.',
     startingStats: { attack: 3, magic: 1, life: 2 },
     productionReady: true,
   },
   {
     id: 'jen_omar',
     displayName: 'Jen & Omar',
-    weaponId: 'smart_tech_stun_baton',
-    passiveId: 'parental_reflexes',
+    weaponId: 'power_tool_wrench',
+    passiveId: 'power_mom',
+    passiveDescription: '+10% move speed and knockback immunity.',
     startingStats: { attack: 1, magic: 2, life: 3 },
     productionReady: true,
   },
   {
     id: 'jason_hilary',
     displayName: 'Jason & Hilary',
-    weaponId: 'river_oak_mash_paddle',
-    passiveId: 'riverbank_endurance',
+    weaponId: 'blueprint_engineering_tool',
+    passiveId: 'engineer_multitool',
+    passiveDescription: '+2 maximum HP and passive health regeneration.',
     startingStats: { attack: 2, magic: 1, life: 3 },
     productionReady: true,
   },
@@ -45,15 +49,17 @@ export const TEAMS: readonly TeamDefinition[] = [
     id: 'joe_cia',
     displayName: 'Joe & Cia',
     weaponId: 'enchanted_oak_pointer',
-    passiveId: 'academic_insight',
+    passiveId: 'efficient_spellcraft',
+    passiveDescription: 'Spells cost 25% less magic.',
     startingStats: { attack: 1, magic: 3, life: 2 },
     productionReady: true,
   },
   {
     id: 'kris_lea',
     displayName: 'Kris & Lea',
-    weaponId: 'carabiner_trekking_pole',
+    weaponId: 'climbing_rope_and_carabiner',
     passiveId: 'alpine_trekker',
+    passiveDescription: 'Ignores mud and snow speed penalties.',
     startingStats: { attack: 2, magic: 2, life: 2 },
     productionReady: true,
   },
