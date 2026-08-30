aea3ed9fc73ae6021d8c11401249a9473ed8e510
RoodOR-Tech <roodj.or@gmail.com>
2026-08-30 14:02:10 -0700

    Replace route crystals with design artifacts

--- src/scenes/TitleScene.ts
+++ src/scenes/TitleScene.ts
@@ -115,7 +115,7 @@ export class TitleScene extends Phaser.Scene {
     if (!this.status || this.confirmingNewGame) return;
     this.status.setText(
       this.save && this.selectedIndex === 0
-        ? `CRYSTALS ${this.save.relics.length}/5 • LIVES ${this.save.resources.lives}/${this.save.resources.maxLives} • XP ${this.save.stats.experience}`
+        ? `ARTIFACTS ${this.save.relics.length}/5 • LIVES ${this.save.resources.lives}/${this.save.resources.maxLives} • XP ${this.save.stats.experience}`
         : this.save
           ? 'START OVER WITH A NEW FAMILY TEAM'
           : 'BEGIN A NEW ROOD HOLIDAY ADVENTURE',
